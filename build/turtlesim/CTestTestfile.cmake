78f1c3866d885bc92b4eb30b5a4b87acfb4fb091
spacecal <twaddell@berkeley.edu>
2021-09-06 16:47:04 -0700

    example workspace set up, will modify to ours later

---
+++ build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/spacecal/SpaceCal/src/ros_tutorials/turtlesim
+# Build directory: /home/spacecal/SpaceCal/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
