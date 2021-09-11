c6b8b8320de5bc7d60d31de30843bee99df47c11
spacecal <twaddell@berkeley.edu>
2021-09-11 16:34:39 -0700

    still working onn rospy error

--- src/JNode_KeyboardInput/setup.py
+++ src/JNode_KeyboardInput/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        	'JNodeInput = JNode_KeyboardInput.keyboardInput.py:main'
+        	'JNodeInput = JNode_KeyboardInput.keyInputNode:main'
 	],
     },
 )
