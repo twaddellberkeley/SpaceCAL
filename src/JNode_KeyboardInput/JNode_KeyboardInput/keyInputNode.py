deed02a9e5814dda783dc5c5637042b391246ae6
waddellt15 <waddellt15@gmail.com>
2021-09-11 18:37:14 -0700

    updated format

--- src/JNode_KeyboardInput/JNode_KeyboardInput/keyInputNode.py
+++ src/JNode_KeyboardInput/JNode_KeyboardInput/keyInputNode.py
@@ -1,27 +1,42 @@
 #Generic node that will check for various key inputs and communicate them via String
 
-import rospy
+import rclpy
 import st
+from rclpy.node import Node
 from pynput import keyboard
 from std_msgs.msg import String
 import time
-def keyTalker():
-    pub = rospy.Publisher('keyInput',String,queue_size=10)
-    rospy.init_node('keyTalker',anonymous=True)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        with keyboard.Events() as events:
-            event = events.get(1e6)
-            if event.key == keyboard.KeyCode.from_char('w'):
-                pub.publish("w")
-                time.sleep(.1)
-            elif event.key == keyboard.KeyCode.from_char('s'):
-                pub.publish("s")
-                time.sleep(.1)
+class keyTalkerClass(Node):
+    def __init__(self):
+        super().__init__('keyTalker')
+        self.publisher_ = self.create_publisher(String, 'keyinput',10)
+        self.keyTalker()
+
+    def keyTalker():
+        while not rclpy.is_shutdown():
+            with keyboard.Events() as events:
+                event = events.get(1e6)
+                if event.key == keyboard.KeyCode.from_char('w'):
+                    self.publisher_.publish("w")
+                    time.sleep(.1)
+                elif event.key == keyboard.KeyCode.from_char('s'):
+                    self.publisher_.publish("s")
+                    time.sleep(.1)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    publisher = keyTalkerClass()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    publisher.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    try:
-        keyTalker()
-    except rospy.ROSInterruptException:
-        pass
+    main()
