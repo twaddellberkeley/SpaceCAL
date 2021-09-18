fc61c13a9e70904e047d8370303496001cd3b90e
spacecal <twaddell@berkeley.edu>
2021-09-17 22:05:38 -0700

    Adding unit tests

--- src/JNode_MotorController/JNode_MotorController/motorController.py
+++ src/JNode_MotorController/JNode_MotorController/motorController.py
@@ -1,5 +1,26 @@
-#Author: Taylor Waddell
-#Link to tic: pololu.com/docs/0J71/12.9
+# Copyright 2021 UC-Berkeley
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# Author: Taylor Waddell
+# Link to tic: pololu.com/docs/0J71/12.9
 # Uses the smbus2 library to send and receive data from a Tic.
 # Works on Linux with either Python 2 or Python 3.
 #
@@ -12,99 +33,106 @@
 #   the device number of your Tic.
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String 
+from std_msgs.msg import String
 from smbus2 import SMBus, i2c_msg
-#globl vars
-#Tic object
+# globl vars
+# Tic object
 tic = None
-#Pulse to move moto
+# Pulse to move moto
 incrBit = 20000
-#Logger of the node
+# Logger of the node
 logger = None
+# Address and bus of motor controlling
+busNum = 1
+address = 14
 
 
-
-#Tic class with generic motor controls
+# Tic class with generic motor controls
 class TicI2C(object):
-  #Init function, takes bus and address
-  def __init__(self, bus, address):
-    #Bus object and address object passed down
-    self.bus = bus
-    self.address = address
-    #See if we can read a bit to test if motor is open
-    try:
-      self.bus.read_byte_data(self.address,0)
-    except:
-      logger.error("Could not open motor controller on address %d" % self.address)
-      exit(0)
-  # Sends the "Exit safe start" command.
-  def exit_safe_start(self):
-    command = [0x83]
-    write = i2c_msg.write(self.address, command)
-    self.bus.i2c_rdwr(write)
- 
-  # Sets the target position.
-  #
-  # For more information about what this command does, see the
-  # "Set target position" command in the "Command reference" section of the
-  # Tic user's guide.
-  def set_target_position(self, target):
-    command = [0xE0,
-      target >> 0 & 0xFF,
-      target >> 8 & 0xFF,
-      target >> 16 & 0xFF,
-      target >> 24 & 0xFF]
-    write = i2c_msg.write(self.address, command)
-    self.bus.i2c_rdwr(write)
- 
-  # Gets one or more variables from the Tic.
-  def get_variables(self, offset, length):
-    write = i2c_msg.write(self.address, [0xA1, offset])
-    read = i2c_msg.read(self.address, length)
-    self.bus.i2c_rdwr(write, read)
-    return list(read)
- 
-  # Gets the "Current position" variable from the Tic.
-  def get_current_position(self):
-    b = self.get_variables(0x22, 4)
-    position = b[0] + (b[1] << 8) + (b[2] << 16) + (b[3] << 24)
-    if position >= (1 << 31):
-      position -= (1 << 32)
-    return position
+    # Init function, takes bus and address
+    def __init__(self, bus, address):
+        # Bus object and address object passed down
+        self.bus = bus
+        self.address = address
+        # See if we can read a bit to test if motor is open
+        try:
+            self.bus.read_byte_data(self.address, 0)
+        except Exception:
+            logger.error(
+                "Could not open motor controller on address %d" % self.address)
+            exit(0)
+    # Sends the "Exit safe start" command.
 
-#Runs the tic motor with a given position
-def Run(intIn): 
+    def exit_safe_start(self):
+        command = [0x83]
+        write = i2c_msg.write(self.address, command)
+        self.bus.i2c_rdwr(write)
+
+    # Sets the target position.
+    #
+    # For more information about what this command does, see the
+    # "Set target position" command in the "Command reference" section of the
+    # Tic user's guide.
+    def set_target_position(self, target):
+        command = [0xE0,
+                   target >> 0 & 0xFF,
+                   target >> 8 & 0xFF,
+                   target >> 16 & 0xFF,
+                   target >> 24 & 0xFF]
+        write = i2c_msg.write(self.address, command)
+        self.bus.i2c_rdwr(write)
+
+    # Gets one or more variables from the Tic.
+    def get_variables(self, offset, length):
+        write = i2c_msg.write(self.address, [0xA1, offset])
+        read = i2c_msg.read(self.address, length)
+        self.bus.i2c_rdwr(write, read)
+        return list(read)
+
+    # Gets the "Current position" variable from the Tic.
+    def get_current_position(self):
+        b = self.get_variables(0x22, 4)
+        position = b[0] + (b[1] << 8) + (b[2] << 16) + (b[3] << 24)
+        if position >= (1 << 31):
+            position -= (1 << 32)
+        return position
+
+# Runs the tic motor with a given position
+
+
+def Run(intIn):
     tic.exit_safe_start()
-    tic.set_target_position(intIn) 
-    
-class keySubscriber(Node):
-  def __init__(self):
-    #Create the subscriber node that listens to key input
-    super().__init__('KeySubscriber')
-    self.subscription = self.create_subscription(
-        String,
-        'keyinput',
-        self.checkRun,
-        10)
-    self.subscription  # prevent unused variable warnings
-    #edit global logger object with node logger
-    global logger
-    logger = self.get_logger()
-    #Create tic object with with motor on /dev/i2c-1
-    bus = SMBus(1)    
-    # Select the I2C address of the Tic (the device number).
-    address = 14
-    global tic 
-    tic = TicI2C(bus, address)
+    tic.set_target_position(intIn)
 
-#Checks current position and message data and sends command accordingly
-  def checkRun(self, msg):
-    #get current position, increase if w, decrease if s
-    curPosition = tic.get_current_position()
-    if (msg.data == 'w'):
-        Run(curPosition + incrBit)
-    elif(msg.data == 's'):
-        Run(curPosition - incrBit)
+
+class keySubscriber(Node):
+    def __init__(self):
+        # Create the subscriber node that listens to key input
+        super().__init__('KeySubscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'keyinput',
+            self.checkRun,
+            10)
+        self.subscription  # prevent unused variable warnings
+        # edit global logger object with node logger
+        global logger
+        logger = self.get_logger()
+        # Create tic object with with motor on /dev/i2c-1
+        bus = SMBus(busNum)
+        # Select the I2C address of the Tic (the device number).
+        global tic
+        tic = TicI2C(bus, address)
+
+# Checks current position and message data and sends command accordingly
+    def checkRun(self, msg):
+        # get current position, increase if w, decrease if s
+        curPosition = tic.get_current_position()
+        if (msg.data == 'w'):
+            Run(curPosition + incrBit)
+        elif(msg.data == 's'):
+            Run(curPosition - incrBit)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -116,9 +144,20 @@ def main(args=None):
     subscriber.destroy_node()
     rclpy.shutdown()
 
-#Main function 
+
+def motorConnectTest():
+    try:
+        bus = SMBus(busNum)
+        # Select the I2C address of the Tic (the device number).
+        bus.read_byte_data(address, 0)
+        return 0
+    except Exception:
+        return 1
+
+
+# Main function
 if __name__ == '__main__':
     try:
         main()
-    except:
-      pass
+    except Exception:
+        pass
