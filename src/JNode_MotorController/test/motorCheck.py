ee66ccae38d770777349adeb1b210ea182834804
spacecal <twaddell@berkeley.edu>
2021-09-20 22:24:56 -0700

    testing added succesfully

--- src/JNode_MotorController/test/motorCheck.py
+++
@@ -1,7 +0,0 @@
-from ..JNode_MotorController.motorController import motorConnectTest
-# Check the motorcontrollers
-
-
-def test_motorConnected():
-    rc = motorConnectTest()
-    assert rc == 0, 'Could not connect to Motor'
