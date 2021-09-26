5e3e4311787f2eb17fda14585f613e05fe78fd0c
spacecal <twaddell@berkeley.edu>
2021-09-26 14:55:22 -0700

    pushing for raspberry pi

--- src/JNode_MotorController/JNode_MotorController/motorController.py
+++ src/JNode_MotorController/JNode_MotorController/motorController.py
@@ -215,10 +215,10 @@ class keySubscriber(Node):
         curPosition = tic.get_current_position()
         if (msg.data == 'w'):
             tic.exit_safe_start()
-            tic.set_target_position(1000)
-        #elif(msg.data == 's'):
-            #tic.exit_safe_start()
-            #tic.set_target_position(curPosition - incrBit)
+            tic.set_target_position(curPosition + incrBit)
+        elif(msg.data == 's'):
+            tic.exit_safe_start()
+            tic.set_target_position(curPosition - incrBit)
 
     # Make tic go to current position
     def go_to(self, msg):
