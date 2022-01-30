8b3b59fdba67f78975810a956a7616c0d2d591e6
spacecal <twaddell@berkeley.edu>
2022-01-29 21:39:41 -0800

    .

--- src/JNode_MotorController/JNode_MotorController/motorController.py
+++ src/JNode_MotorController/JNode_MotorController/motorController.py
@@ -291,8 +291,8 @@ class keySubscriber(Node):
             global address
             motorMsg = MotorData()
             motorMsg.motornum = address
-            motorMsg.speed = tic.get_current_velocity()*velBit
-            motorMsg.location = tic.get_current_position()*incrBit
+            motorMsg.speed = round(tic.get_current_velocity()*velBit)
+            motorMsg.location = round(tic.get_current_position()*incrBit)
             motorMsg.status = tic.get_current_status()
             motorMsg.flags = tic.get_current_flags()
             self.motorDataPublisher.publish(motorMsg)
