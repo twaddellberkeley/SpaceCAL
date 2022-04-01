abaf7f018ebac6eabd754d748147cef81a0a5f51
Christian Castaneda <christianjc_09@yahoo.com>
2022-04-01 13:22:43 -0700

    testing

--- src/jetsonDisplay/jetsonDisplay/spaceCalApp_v2_ros.py
+++ src/jetsonDisplay/jetsonDisplay/spaceCalApp_v2_ros.py
@@ -221,9 +221,10 @@ class UI(QMainWindow):
         self.msgConfirm.setIcon(QMessageBox.Question)
         self.msgConfirm.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
         self.msgConfirm.setDefaultButton(QMessageBox.Ok)
-        self.msgConfirm.buttons().setTex('OK')
+
         print(self.msgConfirm.buttons()[0].text())
         print(self.msgConfirm.buttons()[1].text())
+        self.msgConfirm.buttons()[0].setTex('OK')
         self.msgConfirm.setStyleSheet(msgStyleSheet)
 
         # Information message
