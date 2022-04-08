bace08bb19aed31ab7721ecf92f22417a29cf239
Christian Castaneda <christianjc_09@yahoo.com>
2022-04-07 21:03:24 -0700

    debugging imu pakage

--- src/bno055_imu_pub/include/bno055_imu_pub/bno055_driver.hpp
+++ src/bno055_imu_pub/include/bno055_imu_pub/bno055_driver.hpp
@@ -41,7 +41,7 @@
 #include "smbus_func.h"
 
 #include "std_msgs/msg/string.hpp"
-#include "interfaces/msg/motorData.hpp"
+#include "interfaces/msg/MotorData.hpp"
 #include "interfaces/msg/fusionImu.hpp"
 #include "interfaces/msg/rawImu.hpp"
 
