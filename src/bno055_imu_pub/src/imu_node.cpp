926e16d7c1a0e7626bdc9aa7625daad31b73ee4d
Christian Castaneda <christianjc_09@yahoo.com>
2022-04-07 22:12:19 -0700

    debugging imu pakage

--- src/bno055_imu_pub/src/imu_node.cpp
+++ src/bno055_imu_pub/src/imu_node.cpp
@@ -20,19 +20,19 @@ public:
   // string::string topic_name = "imu_topic";
 
   ImuPublisher()
-      : Node("imu_publisher"), count_(0)
+      : Node("imu_publisher"), count_(0), imu_fusion_("/dev/i2c-1", 0x28, bno055_imu::OPERATION_MODE_IMUPLUS), imu_raw_("/dev/i2c-1", 0x29, bno055_imu::OPERATION_MODE_ACCGYRO)
   {
     // imu_fusion_ = bno055_imu::BNO055Driver("/dev/i2c-1", 0x28, bno055_imu::OPERATION_MODE_IMUPLUS);
     // imu_raw_ = bno055_imu::BNO055Driver("/dev/i2c-1", 0x29, bno055_imu::OPERATION_MODE_ACCGYRO);
-    // imu_fusion_.init();
-    // imu_raw_.init();
+    imu_fusion_.init();
+    imu_raw_.init();
 
-    // fusion_publisher_ = this->create_publisher<std_msgs::msg::String>("fusion_imu_topic", 10);
-    // raw_publisher_ = this->create_publisher<std_msgs::msg::String>("raw_imu_topic", 10);
-    // fusion_timer_ = this->create_wall_timer(
-    //     500ms, std::bind(&ImuPublisher::fusion_callback, this));
-    // raw_timer_ = this->create_wall_timer(
-    //     500ms, std::bind(&ImuPublisher::raw_callback, this));
+    fusion_publisher_ = this->create_publisher<std_msgs::msg::String>("fusion_imu_topic", 10);
+    raw_publisher_ = this->create_publisher<std_msgs::msg::String>("raw_imu_topic", 10);
+    fusion_timer_ = this->create_wall_timer(
+        500ms, std::bind(&ImuPublisher::fusion_callback, this));
+    raw_timer_ = this->create_wall_timer(
+        500ms, std::bind(&ImuPublisher::raw_callback, this));
   }
 
 private:
