1a7465582b4cb961073cf5219eb19704edbbb18a
Christian Castaneda <christianjc_09@yahoo.com>
2022-04-09 12:24:52 -0700

    debugging

--- src/bag_recorder_cpp/src/topic_recorder.cpp
+++ src/bag_recorder_cpp/src/topic_recorder.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
-#include <ctime>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
-#include <interfaces/msg/fusion_imu.hpp>
-#include <interfaces/msg/raw_imu.hpp>
-#include <interfaces/msg/motor_data.hpp>
-#include <interfaces/msg/display_data.hpp>
 
+#include <rosbag2_cpp/typesupport_helpers.hpp>
 #include <rosbag2_cpp/writer.hpp>
+#include <rosbag2_cpp/writers/sequential_writer.hpp>
+#include <rosbag2_storage/serialized_bag_message.hpp>
 
 using std::placeholders::_1;
 
@@ -15,44 +12,57 @@ class BagRecorder : public rclcpp::Node
 {
 public:
   BagRecorder()
-      : Node("bag_recorder")
+  : Node("bag_recorder")
   {
-    time_t ttime = time(0);
-    // tm *local_time = localtime(&ttime);
-    std::string data = ctime(&ttime);
-    std::string time = "my_bag/" + data;
-    const rosbag2_cpp::StorageOptions storage_options({time, "sqlite3"});
+    const rosbag2_cpp::StorageOptions storage_options({"my_bag2", "sqlite3"});
     const rosbag2_cpp::ConverterOptions converter_options(
-        {rmw_get_serialization_format(),
-         rmw_get_serialization_format()});
-
-    writer_ = std::make_unique<rosbag2_cpp::Writer>();
+      {rmw_get_serialization_format(),
+       rmw_get_serialization_format()});
+    writer_ = std::make_unique<rosbag2_cpp::writers::SequentialWriter>();
 
     writer_->open(storage_options, converter_options);
 
     writer_->create_topic(
-        {"buttons_topic",
-         "std_msgs/msg/String",
-         rmw_get_serialization_format(),
-         ""});
+      {"buttons_topic",
+       "std_msgs/msg/String",
+       rmw_get_serialization_format(),
+       ""});
 
     subscription_ = create_subscription<std_msgs::msg::String>(
-        "buttons_topic", 10, std::bind(&BagRecorder::topic_callback, this, _1));
+      "buttons_toppic", 10, std::bind(&BagRecorder::topic_callback, this, _1));
   }
 
 private:
   void topic_callback(std::shared_ptr<rclcpp::SerializedMessage> msg) const
   {
-    rclcpp::Time time_stamp = this->now();
+    auto bag_message = std::make_shared<rosbag2_storage::SerializedBagMessage>();
 
-    writer_->write(*msg, "buttons_topic", "std_msgs/msg/String", time_stamp);
+    bag_message->serialized_data = std::shared_ptr<rcutils_uint8_array_t>(
+      new rcutils_uint8_array_t,
+      [this](rcutils_uint8_array_t *msg) {
+        auto fini_return = rcutils_uint8_array_fini(msg);
+        delete msg;
+        if (fini_return != RCUTILS_RET_OK) {
+          RCLCPP_ERROR(get_logger(),
+            "Failed to destroy serialized message %s", rcutils_get_error_string().str);
+        }
+      });
+    *bag_message->serialized_data = msg->release_rcl_serialized_message();
+
+    bag_message->topic_name = "buttons_topic";
+    if (rcutils_system_time_now(&bag_message->time_stamp) != RCUTILS_RET_OK) {
+      RCLCPP_ERROR(get_logger(), "Error getting current time: %s",
+        rcutils_get_error_string().str);
+    }
+
+    writer_->write(bag_message);
   }
 
   rclcpp::Subscription<rclcpp::SerializedMessage>::SharedPtr subscription_;
-  std::unique_ptr<rosbag2_cpp::Writer> writer_;
+  std::unique_ptr<rosbag2_cpp::writers::SequentialWriter> writer_;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<BagRecorder>());
