3ecd39684e7e2da24ebe83372e5116baeee76d69
Christian Castaneda <christianjc_09@yahoo.com>
2022-04-12 13:43:23 -0700

    added a new package for main logic implementation

---
+++ src/jetson_main_logic/jetson_main_logic/main_logic.py
@@ -0,0 +1,14 @@
+
+
+# This node takes input from the jetson Nano display and performs the required command.
+# There are a total of 9 motors and 5 projectors that need to be control. 
+
+# Actions that need to be performed
+
+
+def main():
+    print('Hi from jetson_main_logic.')
+
+
+if __name__ == '__main__':
+    main()
