41c7a0ea3e7fad480cecf18969dece3c292f0e4c
Christian Castaneda <christianjc_09@yahoo.com>
2022-04-21 19:43:58 -0700

    new logic

--- src/jetson_main_logic/jetson_main_logic/test_srv.py
+++ src/jetson_main_logic/jetson_main_logic/test_srv.py
@@ -11,8 +11,8 @@ class MinimalService(Node):
         super().__init__('minimal_service')
         self.motor_srv = self.create_service(
             MotorSrv, 'motor_command_1', self.motor_command)
-        # self.projector_srv = self.create_service(
-        #     ProjectorSrv, 'projector_command_1', self.projector_command)
+        self.projector_srv = self.create_service(
+            ProjectorSrv, 'projector_command_1', self.projector_command)
 
     def motor_command(self, request, response):
         self.get_logger().info('Incoming request\na: %d b: %d' %
@@ -24,15 +24,15 @@ class MinimalService(Node):
                                (response.ok, response.error))
         return response
 
-    # def projector_command(self, request, response):
-    #     self.get_logger().info('Incoming request\na: %d b: %d' %
-    #                            (request.cmd_num, request.value))
-    #     response.ok = True
-    #     response.error = 10
-    #     time.sleep(3)
-    #     self.get_logger().info('After 3 seconds \na: %d b: %d' %
-    #                            (response.ok, response.error))
-    #     return response
+    def projector_command(self, request, response):
+        self.get_logger().info('Incoming request\na: %d b: %d' %
+                               (request.cmd_num, request.value))
+        response.ok = True
+        response.error = 10
+        time.sleep(3)
+        self.get_logger().info('After 3 seconds \na: %d b: %d' %
+                               (response.ok, response.error))
+        return response
 
 
 def main(args=None):
