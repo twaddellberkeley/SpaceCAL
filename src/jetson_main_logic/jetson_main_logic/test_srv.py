7d547fca91f1bf30f46b495b793c14566c30977a
Christian Castaneda <christianjc_09@yahoo.com>
2022-04-21 19:22:07 -0700

    new logic

--- src/jetson_main_logic/jetson_main_logic/test_srv.py
+++ src/jetson_main_logic/jetson_main_logic/test_srv.py
@@ -10,11 +10,21 @@ class MinimalService(Node):
     def __init__(self):
         super().__init__('minimal_service')
         self.motor_srv = self.create_service(
-            MotorSrv, 'motor_command_1', self.print_command)
+            MotorSrv, 'motor_command_1', self.motor_command)
         self.projector_srv = self.create_service(
-            ProjectorSrv, 'projector_command_1', self.print_command)
+            ProjectorSrv, 'projector_command_1', self.projecotr_command)
 
-    def print_command(self, request, response):
+    def motor_command(self, request, response):
+        self.get_logger().info('Incoming request\na: %d b: %d' %
+                               (request.cmd_num, request.value))
+        response.ok = True
+        response.error = 10
+        time.sleep(3)
+        self.get_logger().info('After 3 seconds \na: %d b: %d' %
+                               (response.ok, response.error))
+        return response
+
+    def projector_command(self, request, response):
         self.get_logger().info('Incoming request\na: %d b: %d' %
                                (request.cmd_num, request.value))
         response.ok = True
