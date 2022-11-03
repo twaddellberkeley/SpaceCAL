bd9657e6f072323ad891c0fca3a077b8b012accb
spacecal <twaddell@berkeley.edu>
2022-11-03 15:06:36 -0700

    connected gui

--- src/main_gui/main_gui/main_gui_node.py
+++ src/main_gui/main_gui/main_gui_node.py
@@ -12,38 +12,47 @@ from .submodules.mainwindow import MainWindow
 from interfaces.srv import GuiDisplay, GuiInput, Projector, Video, MotorSrv
 
 
-class GuiNode():
+class GuiNode(Node):
 
     def __init__(self, window):
-        super().__init__()
-        rclpy.init(args=None)
+        super().__init__('main_gui_server_display_node')
+        # 'main_gui_server_node'
+        
         self.gui = window
+        serverThread = threading.Thread(target=self.exec_gui_node)
+        serverThread.start()
+        
 
     def exec_gui_node(self):
-        subNode = Node('main_gui_server_node')
+        subNode = Node('main_gui_server_display_node')
         #***** Servers ******#
-        self.gui_srv = self.create_service(
-            GuiDisplay, 'gui_input_srv', self.gui_input_callback)
-
+        server = subNode.create_service(
+            GuiDisplay, 'gui_display_srv', self.gui_display_callback)
         rclpy.spin(subNode)
         subNode.destroy_node()
+        
 
-    def gui_display_callback(slef):
-        pass
+    def gui_display_callback(self, req, res):
+
+        self.gui.sendCmd("signal_display_input")
+        res.cmd = "Hello"
+        return res
+        
 
 
 def main(args=None):
     os.environ['DISPLAY'] = ":0"
+    rclpy.init(args=None)
     app = QApplication(sys.argv)
     window = MainWindow()
     gui = GuiNode(window)
     gui.gui.show()
     e = app.exec()
-    window.pubNode.destroy_node()
-    rclpy.shutdown()
+    # window.pubNode.destroy_node()
+   
     # rclpy.shutdown()
     sys.exit(e)
-
+    rclpy.shutdown()
     # rclpy.init(args=args)
     # gui = GuiNode()
     # print('Hi from GUI %d.', gui.motor_num)
